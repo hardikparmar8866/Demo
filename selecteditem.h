95fec0d853d9cf07290d47230c1c652e133d9011
hardikparmar8866 <hardikparmar8866826931@gmail.com>
2017-12-16 08:43:40 +0530

    Create selecteditem.h

---
+++ selecteditem.h
@@ -0,0 +1,11 @@
+
+
+#import <Foundation/Foundation.h>
+
+@interface selecteditem : NSObject
+@property(nonatomic, strong)NSIndexPath *indexPath;
+@property(nonatomic, strong)NSString *ID;
+@property(nonatomic, strong)NSString *name;
+@property(nonatomic, strong)NSString *price;
+
+@end
