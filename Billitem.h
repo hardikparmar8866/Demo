ce8824629625cb5795f6be07dd2ddd1b9f703a06
hardikparmar8866 <hardikparmar8866826931@gmail.com>
2017-12-16 08:42:49 +0530

    Create Billitem.h

---
+++ Billitem.h
@@ -0,0 +1,11 @@
+
+
+#import <Foundation/Foundation.h>
+
+@interface Billitem : NSObject
+
+@property(nonatomic, strong)NSString *ID;
+@property(nonatomic, strong)NSString *name;
+@property(nonatomic, strong)NSString *price;
+@property(nonatomic, strong)NSString *qty;
+@end
